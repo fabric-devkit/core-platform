0ad56ff0d6bcbea08124125c84067e48e9fcd174
Paul Sitoh <paulwizviz@gmail.com>
2019-08-31 22:54:39 +0100

    Elevate networks artifacts to top level and other
    - Move and rename dev to addorg
    - Move Kafka to top level and fix bug
    - Move README and guides to docs
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- kafka/fabricOps.sh
+++ kafka/fabricOps.sh
@@ -56,7 +56,7 @@ function kafka(){
 function createCryptoChannelArtefacts(){
     rm -rf ./channel-artefacts
     rm -rf ./crypto-config
-    docker run --rm -e "GOPATH=/opt/gopath" -e "FABRIC_CFG_PATH=/opt/gopath/src/github.com/hyperledger/fabric" -w="/opt/gopath/src/github.com/hyperledger/fabric" --volume=${PWD}:/opt/gopath/src/github.com/hyperledger/fabric hyperledger/fabric-tools /bin/bash -c '${PWD}/generate-artefacts.sh'
+    docker run --rm -e "GOPATH=/opt/gopath" -e "FABRIC_CFG_PATH=/opt/gopath/src/github.com/hyperledger/fabric" -w="/opt/gopath/src/github.com/hyperledger/fabric" --volume=${PWD}:/opt/gopath/src/github.com/hyperledger/fabric hyperledger/fabric-tools:1.4 /bin/bash -c '${PWD}/generate-artefacts.sh'
 }
 
 function renameSecretPrivKeys(){
